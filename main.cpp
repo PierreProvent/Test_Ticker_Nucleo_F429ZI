89d8ce4c3fb874a89c996c6d8e994613889c1c14
pierreprovent <PierreProvent@users.noreply.github.com>
2020-09-16 13:15:42 +0000

    Fichier source pour cours ELE118 Cnam

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+/*-----------------------------------------------------------------------
+Before to use this example, ensure that you an hyperterminal installed on your computer. 
+More info here: https://developer.mbed.org/handbook/Terminals
+The default serial comm port uses the SERIAL_TX and SERIAL_RX pins (see their definition in the PinNames.h file).
+The default serial configuration in this case is 9600 bauds, 8-bit data, no parity. 
+If you want to change the baudrate for example, you have to redeclare the
+serial object in your code:
 
-int main() {
+Serial pc(SERIAL_TX, SERIAL_RX);
+
+Then, you can modify the baudrate and print like this:
+
+pc.baud(115200);
+pc.printf("Hello World !\n");
+-------------------------------------------------------------------------*/
+
+DigitalOut led(LED1);
+
+int main()
+{
+    int i = 1;
+
+    printf("Hello World !\n");
+
     while(1) {
-        myled = 1; // LED is ON
-        wait(0.2); // 200 ms
-        myled = 0; // LED is OFF
-        wait(1.0); // 1 sec
+        wait(1); // 1 second
+        led = !led; // Toggle LED
+        printf("This program runs since %d seconds.\n", i++);
     }
 }
